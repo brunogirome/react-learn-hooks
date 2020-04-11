51c3531be40fd6d0b92d90b941bda7482834eca1
Bruno Girome <brunogirome@gmail.com>
2020-04-11 18:51:56 -0300

    useState basics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
-  return <h1>Hello world</h1>;
+  const [bands, setBand] = useState(['Ghost', 'Slayer', 'Kreator']);
+
+  const [newBand, setNewBand] = useState('');
+
+  function handleAdd() {
+    setBand([...bands, newBand]);
+    setNewBand('');
+  }
+
+  return (
+    <>
+      <ul>
+        {bands.map(band => (
+          <li key={band}>{band}</li>
+        ))}
+        <input
+          type="text"
+          value={newBand}
+          onChange={e => setNewBand(e.target.value)}
+        />
+        <button type="button" onClick={handleAdd}>
+          Add a band
+        </button>
+      </ul>
+    </>
+  );
 }
